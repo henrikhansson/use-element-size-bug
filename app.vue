175934a20d98431a9bea392a83ab8f82e3215eb3
Henrik Langer-Hansson <henrik@Oxys-Hauch.local>
2023-08-21 10:43:40 +0200

    init commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div ref="container">
+    <h1>{{ width }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const container = ref();
+const { width } = useElementSize( container );
+
+onMounted(() => {
+  console.log(`VueUse: ${ width.value }`);
+  console.log(`offsetWidth: ${ container.value.offsetWidth }`);
+});
+
+</script>
